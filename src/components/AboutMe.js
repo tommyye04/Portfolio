7395f05a8027c1533608ef594068714b79c743d0
tommyye04 <tye47@uwo.ca>
2024-01-15 15:15:27 -0500

    about me update

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -10,7 +10,22 @@ const AboutMe = () => {
         <h2>About Me</h2>
         <div className="text-box">
           <p>
-            Hello! I'm Tommy, a current UWO Software Engineering Undergraduate.
+            <h3>Welcome to my Portfolio</h3>
+            <p>
+              Hey there! I'm Tommy Ye, a current 2nd year Software Engineering
+              undergraduate at UWO.
+            </p>
+            <h3>What drives me</h3>{" "}
+            <p>
+              I'm not just here to earn a degree; I'm passionate about
+              engineering. Whether it's delving into the intricacies of software
+              and programming or getting involved in the various clubs on
+              campus, every day is a chance to grow and learn.
+            </p>
+            I'm always open to new opportunities, collaborations, or just a
+            friendly chat. Whether you're a fellow student, a potential
+            collaborator, or someone passionate about software engineering, I'd
+            love to hear from you. <p>Cheers, </p> <p>Tommy</p>
           </p>
         </div>
       </div>
